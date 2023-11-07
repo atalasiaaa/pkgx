3f320133104e37790a7079dcdd4b5b135f4168d9
Atalasia Minun <90100836+atalasiaaa@users.noreply.github.com>
2023-11-07 05:38:11 +0000

    Test Tea

---
+++ test.py
@@ -0,0 +1,14 @@
+# Input data
+name = "Atalasia Minun"
+email = "atalasiaminun@gmail.com"
+github = "https://github.com/atalasiaaa"
+twitter = "@Atalasiaa"
+discord = "atalasia"
+
+# Display information
+print("Name: " + name)
+print("Email: " + email)
+print("GitHub: " + github)
+print("Twitter: " + twitter)
+print("Discord: " + discord)
+
